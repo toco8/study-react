88a86fd8229a9701c2eab020dfc5081c30d93995
TAK <39761181+toco8@users.noreply.github.com>
2022-05-11 21:58:03 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export const App = () => {
+  const onClickButton = () => {
+    alert();
+  };
+
+  return (
+    <>
+      <h1>こんにちは！</h1>
+      <p>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
